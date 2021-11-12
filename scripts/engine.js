039408bb2ae968c5dc2fb1367e29e461e4e212de
Barry Hess <barry@bjhess.com>
2021-11-12 12:17:55 -0600

    functionize the even-odd loading.

--- scripts/engine.js
+++ scripts/engine.js
@@ -1,22 +1,26 @@
 "use strict";
 import * as Worker from './worker.js';
 
-var now = new Date();
-var month = now.getMonth();
-var day = now.getDate();
-var hour = now.getHours();
+function loadEvenOdd() {
+  var now = new Date();
+  var month = now.getMonth();
+  var day = now.getDate();
+  var hour = now.getHours();
 
-// Current date and time in Owatonna
-var nowString = 
-  now.toLocaleTimeString("en-US", Worker.timeOptions) +
-  " on " +
-  now.toLocaleDateString("en-US", Worker.dateOptions);
+  // Current date and time in Owatonna
+  var nowString = 
+    now.toLocaleTimeString("en-US", Worker.timeOptions) +
+    " on " +
+    now.toLocaleDateString("en-US", Worker.dateOptions);
 
-window.onload = function() {
   var parkingDeets = Worker.parkingDeets(now);
 
   document.getElementById("now").innerHTML = nowString;
   document.getElementById("park").innerHTML = parkingDeets.get('park');
   document.getElementById("parkArticle").innerHTML = parkingDeets.get('parkArticle');
   document.getElementById("parkExplanation").innerHTML = parkingDeets.get('parkExplanation');
-};
+}
+
+window.onload = function() {
+  loadEvenOdd();
+}
